308b9e052b653968d21e023806696b388b80d4d3
Jonnier Andres Teran Morales <jonnierteranmorales@gmail.com>
2022-11-04 00:19:47 -0500

    Aplicacion de metodos Update y delete a la base de datos por medio de httpClient

--- src/app/Services/Data.service.ts
+++ src/app/Services/Data.service.ts
@@ -23,22 +23,49 @@ export class DataService{
     }
 
     public GetPersonas():PersonaModel[]{
-      
        this.httpClient.get<PersonaModel[]>('https://registro-de-personal-3bae4-default-rtdb.firebaseio.com/Data.json')
          .subscribe(
             (Response:PersonaModel[]) =>
             {
-                Response.forEach(Element =>{
+                Response.forEach(Element  =>{
                     this.ArregloP.push(Element);
+                    
                 })
+                
+                alert("El Personal Registrado en la base de datos fue Cargado Exitosamente!")
             }, Error =>{
-               Error
+               console.log(Error)
             }
        );
 
-       return this.ArregloP;
-       
-           
+       return this.ArregloP;     
 }
+
+    public UpdatePersona(index:number,persona:PersonaModel){
+        let Url:string;
+        Url ='https://registro-de-personal-3bae4-default-rtdb.firebaseio.com/Data/'+ index +'.json';
+        this.httpClient.put(Url , persona).subscribe(
+            Response =>{
+                console.log(Response)
+            }, Error => {
+                console.log(Error)
+                alert("Error Al actualizar")
+            }
+        )
+    }
+
+    public DeletePersona(index:number){
+        let Url:string;
+        Url ='https://registro-de-personal-3bae4-default-rtdb.firebaseio.com/Data/'+ index +'.json';
+        this.httpClient.delete(Url).subscribe(
+            Response =>{
+                console.log(Response);
+            }, Error => {
+                console.log(Error)
+                alert("Error Al Eliminar")
+            }
+        )
+
+    }
 
 }
--- src/app/Services/Persona.service.ts
+++ src/app/Services/Persona.service.ts
@@ -15,10 +15,13 @@ export class PersonaService {
     this.Personas = this._DataService.GetPersonas();
 
   }
+
   public setAgregarPersona(Persona:PersonaModel):void{
       this.Personas.push(Persona);
       this._DataService.RegistrarPersona(this.Personas)
+      alert("Personal Registrado con exito!!");
   }
+  
   public EncontrarPersona(index:number):PersonaModel{
     let Persona: PersonaModel = this.Personas[index];
     return Persona;
@@ -26,9 +29,21 @@ export class PersonaService {
 
   public ActualizarPersona(index:number, Persona:PersonaModel):void{
     this.Personas[index] = Persona; 
+    this._DataService.UpdatePersona(index,Persona)
+    alert('Actualizacion Exitosa')
   }
 
   public EliminarPersona(index:number):void{
     this.Personas.splice(index,1);
+    this._DataService.DeletePersona(index);
+    //Se vuelve a guardar el arreglo
+    this.modificarPersonas();
+    alert('Eliminacion Exitosa')
+  }
+
+  public modificarPersonas(){
+    if(this.Personas != null){
+      this._DataService.RegistrarPersona(this.Personas);
+    }
   }
 }
